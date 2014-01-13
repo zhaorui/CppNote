4d221a40ca9c36f2363a8fc18ad5a6d37f8c54d5
zhaorui <zhaoruiexplorer@gmail.com>
2014-01-14 06:55:36 +0800

    New example

--- C/long.c
+++ C/long.c
@@ -1,8 +1,21 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <getopt.h>
+
 //extern char *optarg;
 //extern int optind, opterr, optopt;
+
+int dick;
+static struct option long_options[] = {
+    {"ass",no_argument,0,'a'},
+    {"batman",required_argument,0,'b'},
+    {"cox",optional_argument,0,'c'},
+    {"dick",no_argument,&dick,'d'},
+    {0,0,0,0}
+};
+
+const char * options = ":ab:cd";
 
 void inline printf_optarg()
 {
@@ -15,7 +28,7 @@ void inline printf_optarg()
 int main(int argc,char *argv[])
 {
     int ch;
-
+    int option_index = 0;
     printf("++++++ Debug Getopt ++++++\n");
     printf_optarg();
     printf("optind: %d\n",optind);
@@ -23,10 +36,10 @@ int main(int argc,char *argv[])
     printf("optopt: %d\n",optopt);
     printf("++++++++++++++++++++++++++\n\n");
 
-    while((ch=getopt(argc,argv,":a:bcd"))!=-1)
+    while((ch=getopt_long(argc,argv,options,long_options,&option_index))!=-1)
     {
         printf("------------------------\n");
-        printf("ch:     %c\n",(char)ch);
+        printf("ch:     %c %d\n",(char)ch,ch);
         printf_optarg();
         printf("optind: %d\n",optind);
         printf("opterr: %d\n",opterr);
